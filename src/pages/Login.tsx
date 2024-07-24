542b1082d6d3f3481b174bf01b38fc8dab221ebe
mdnazmul42726 <nazmulofficial@outlook.com>
2024-07-24 22:16:06 +0600

    07/24/2024

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,40 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { FC } from "react";
+import { useAppDispatch } from "../redux/hook";
+import { setUser } from "../redux/features/auth/authSlice";
 
-const Login = () => {
+const Login: FC = () => {
+    const { register, handleSubmit } = useForm();
+    const dispatch = useAppDispatch();
+
+    const [login, { error, isLoading }] = useLoginMutation();
+
+
+    async function onSubmit(data) {
+        const res = await login(data).unwrap();
+
+        if (!isLoading && !error) {
+            dispatch(setUser({ user: {}, token: res.data.accessToken }))
+
+        }
+    }
+
     return (
         <div>
-            this is login comp
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="">
+                    <label htmlFor="id">ID: </label>
+                    <input type="text" id="id" {...register('id')} />
+                </div>
+
+                <div className="">
+                    <label htmlFor="id">Password: </label>
+                    <input type="text" id="password" {...register('password')} />
+                </div>
+                <Button htmlType="submit">Login</Button>
+            </form>
         </div>
     );
 };
---
+++ src/utils/verifyToken.ts
@@ -0,0 +1,4 @@
+
+export function verifyToken(token: string) {
+    return
+}
