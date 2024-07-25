e312dded4bed021484440dbf255b09643dcbf23a
mdnazmul42726 <nazmulofficial@outlook.com>
2024-07-25 11:27:20 +0600

    specifically ignore all the action types it dispatches:

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './features/auth/authSlice';
 import { baseApi } from './api/baseApi';
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 const persistConfig = { key: 'auth', storage };
@@ -12,7 +12,11 @@ export const store = configureStore({
         [baseApi.reducerPath]: baseApi.reducer,
         auth: persistedReducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }).concat(baseApi.middleware),
 })
 
 export const persistor = persistStore(store);
